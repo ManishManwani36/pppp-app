5a6121efe40b59c1acfe9dca61118438d904b137
ManishManwani36 <manishmanwani73@gmail.com>
2024-05-29 15:44:04 +0930

    Added nav items

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -6,9 +6,76 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { Menu } from "lucide-react";
+import {
+  Cable,
+  Cctv,
+  Cog,
+  FileBadge,
+  FileSearch2,
+  FolderTree,
+  Menu,
+  MoveHorizontal,
+  NotebookPen,
+  PackageMinus,
+  PackagePlus,
+  ScanBarcode,
+  Send,
+  ShieldAlert,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
 
-export function MobileNavigation() {
+const NavigationLinks = [
+  {
+    name: "Monitoring",
+    icon: Cctv,
+  },
+  {
+    name: "Holding Search",
+    icon: FolderTree,
+  },
+  {
+    name: "E-Guide",
+    icon: FileSearch2,
+  },
+  {
+    name: "Relics Register",
+    icon: ScanBarcode,
+  },
+  {
+    name: "Return",
+    icon: NotebookPen,
+  },
+  {
+    name: "Move In",
+    icon: PackagePlus,
+  },
+  {
+    name: "Settle In",
+    icon: Send,
+  },
+  {
+    name: "Lend",
+    icon: MoveHorizontal,
+  },
+  {
+    name: "Move Out",
+    icon: PackageMinus,
+  },
+  {
+    name: "Damage Register",
+    icon: ShieldAlert,
+  },
+  {
+    name: "Connection",
+    icon: Cable,
+  },
+  {
+    name: "Settings",
+    icon: Cog,
+  },
+];
+
+export function SideNavigation() {
   return (
     <div>
       <Sheet>
@@ -20,16 +87,19 @@ export function MobileNavigation() {
         </div>
         <SheetContent side="left">
           <SheetHeader className="text-left">
-            <SheetTitle>Menu</SheetTitle>
+            <SheetTitle className="ml-4">Menu</SheetTitle>
             <SheetDescription>
-              <ul>
-                <li>Item</li>
-                <li>Item</li>
-                <li>Item</li>
-                <li>Item</li>
-                <li>Item</li>
-                <li>Item</li>
-              </ul>
+              <div className="gap-3">
+                {NavigationLinks.map((link) => (
+                  <Button
+                    key={link.name}
+                    className="flex items-center justify-start gap-2 w-full"
+                    variant="ghost">
+                    <link.icon />
+                    <span>{link.name}</span>
+                  </Button>
+                ))}
+              </div>
             </SheetDescription>
           </SheetHeader>
         </SheetContent>
